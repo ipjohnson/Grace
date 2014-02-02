a20719327c66e6ca0f050894ceaebbb66215a26b
ipjohnson <ipjohnson123@yahoo.com>
2014-02-02 12:33:13 -0700

    Adding AsElement to repo

---
+++ Source/Grace.Net/DependencyInjection/Configuration/AsElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grace.DependencyInjection.Configuration;
+
+namespace Grace.DependencyInjection.Configuration
+{
+	public class AsElement : BaseElement
+	{
+		[ConfigurationProperty("type", IsRequired = false)]
+		public string Type
+		{
+			get { return PropertyValue<string>(); }
+		}
+
+		[ConfigurationProperty("name", IsRequired = false)]
+		public string Name
+		{
+			get { return PropertyValue<string>(); }
+		}
+	}
+}
