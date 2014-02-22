c574598afa2bb8215a8c609774b736901df29837
ipjohnson <ipjohnson123@yahoo.com>
2014-02-22 12:41:37 -0700

    Adding comments

--- Source/Grace/DependencyInjection/IFluentImportPropertyCollectionConfiguration.cs
+++ Source/Grace/DependencyInjection/IFluentImportPropertyCollectionConfiguration.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 
 namespace Grace.DependencyInjection
 {
+	/// <summary>
+	/// Configuration interface for exporting a collection
+	/// </summary>
+	/// <typeparam name="T">exported type</typeparam>
+	/// <typeparam name="TItem">property collection type</typeparam>
 	public interface IFluentImportPropertyCollectionConfiguration<T, out TItem> : IFluentExportStrategyConfiguration<T>
 	{
+		/// <summary>
+		/// filter to be used when filling collection
+		/// </summary>
+		/// <param name="consider">filter delegate</param>
+		/// <returns>configuration object</returns>
 		IFluentImportPropertyCollectionConfiguration<T, TItem> Consider(ExportStrategyFilter consider);
 
+		/// <summary>
+		/// Sort the collection by property
+		/// </summary>
+		/// <param name="propertyFunc">function to retrieve property</param>
+		/// <returns>configuration object</returns>
 		IFluentImportPropertyCollectionConfiguration<T, TItem> SortByProperty(Func<TItem, IComparable> propertyFunc);
 
+		/// <summary>
+		/// Sort the collection using an IComparer
+		/// </summary>
+		/// <param name="comparer">comparer</param>
+		/// <returns>configuration object</returns>
 		IFluentImportPropertyCollectionConfiguration<T, TItem> SortBy(IComparer<TItem> comparer);
 	}
 }
