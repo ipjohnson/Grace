f21ab2aa6a6a2951b121ea6b3237cf07d53a2179
ipjohnson <ipjohnson123@yahoo.com>
2014-03-01 20:14:41 -0700

    Adding missing files

---
+++ Source/Grace.UnitTests/Classes/Simple/CircularDependencyClasses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace.UnitTests.Classes.Simple
+{
+	public interface IDependencyA
+	{
+		
+	}
+
+	public interface IDependencyB
+	{
+		
+	}
+
+	public class DependencyA : IDependencyA
+	{
+		public DependencyA(IDependencyB dependencyB)
+		{
+			
+		}
+	}
+
+	public class DependencyB : IDependencyB
+	{
+		public DependencyB(IDependencyA dependencyA)
+		{
+			
+		}
+	}
+}
