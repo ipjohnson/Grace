d98972826a3b1e0b452d85a5fd0e8ec5187dc60d
ipjohnson <ipjohnson123@yahoo.com>
2014-05-25 13:44:59 -0600

    Adding new WeakAction & WeakFunc

--- Source/Grace/Utilities/WeakAction.cs
+++ Source/Grace/Utilities/WeakAction.cs
@@ -69,4 +69,73 @@ namespace Grace.Utilities
 			}
 		}
 	}
+
+	/// <summary>
+	/// Weak Action&lt;T&gt;
+	/// </summary>
+	/// <typeparam name="T">action param</typeparam>
+	public class WeakAction<T>
+	{
+		private readonly Action<object, T> executeAction;
+		private readonly WeakReference reference;
+
+		/// <summary>
+		/// Default Action
+		/// </summary>
+		/// <param name="action"></param>
+		public WeakAction(Action<T> action)
+		{
+			if (action.Target != null)
+			{
+				reference = new WeakReference(action.Target);
+
+				MethodInfo methInfo = action.GetMethodInfo();
+
+				ParameterExpression param = Expression.Parameter(typeof(object), "target");
+
+				ParameterExpression tParam = Expression.Parameter(typeof(T), "tParam");
+
+				UnaryExpression cast =
+					Expression.ConvertChecked(param, action.Target.GetType());
+
+				Expression call = Expression.Call(cast, methInfo, tParam);
+
+				executeAction =
+					Expression.Lambda<Action<object, T>>(call, param, tParam).Compile();
+			}
+			else
+			{
+				executeAction = (o, t) => action(t);
+			}
+		}
+
+		/// <summary>
+		/// True if Target is alive
+		/// </summary>
+		public bool IsAlive
+		{
+			get { return reference.IsAlive; }
+		}
+
+		/// <summary>
+		/// Invoke the action
+		/// </summary>
+		public void Invoke(T tParam)
+		{
+			if (reference != null)
+			{
+				object target = reference.Target;
+
+				if (executeAction != null && target != null)
+				{
+					executeAction(target, tParam);
+				}
+			}
+			else
+			{
+				// this is the static case and no object to pass in
+				executeAction(null, tParam);
+			}
+		}
+	}
 }
--- Source/Grace/Utilities/WeakFunc.cs
+++ Source/Grace/Utilities/WeakFunc.cs
@@ -73,4 +73,77 @@ namespace Grace.Utilities
 			return default(TResult);
 		}
 	}
+
+	/// <summary>
+	/// Represents a Func(T) that will not hold a reference to the target
+	/// </summary>
+	/// <typeparam name="TResult"></typeparam>
+	/// <typeparam name="TArg"></typeparam>
+	public class WeakFunc<TArg, TResult>
+	{
+		private readonly Func<object, TArg, TResult> executeAction;
+		private readonly WeakReference reference;
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		/// <param name="func"></param>
+		public WeakFunc(Func<TArg, TResult> func)
+		{
+			if (func.Target != null)
+			{
+				reference = new WeakReference(func.Target);
+
+				MethodInfo methInfo = func.GetMethodInfo();
+
+				ParameterExpression param = Expression.Parameter(typeof(object), "target");
+
+				ParameterExpression tParam = Expression.Parameter(typeof(object), "tArg");
+
+				UnaryExpression cast =
+					Expression.ConvertChecked(param, func.Target.GetType());
+
+				Expression call = Expression.Call(cast, methInfo, tParam);
+
+				executeAction =
+					Expression.Lambda<Func<object, TArg, TResult>>(call, param, tParam).Compile();
+			}
+			else
+			{
+				executeAction = (o, t) => func(t);
+			}
+		}
+
+		/// <summary>
+		/// Is the target alive
+		/// </summary>
+		public bool IsAlive
+		{
+			get { return reference == null || reference.IsAlive; }
+		}
+
+		/// <summary>
+		/// Invoke the Func
+		/// </summary>
+		/// <returns></returns>
+		public TResult Invoke(TArg tArg)
+		{
+			if (reference != null)
+			{
+				object objectRef = reference.Target;
+
+				if (objectRef != null)
+				{
+					return executeAction(objectRef, tArg);
+				}
+			}
+			else
+			{
+				// this is the static method case and there is no instance
+				return executeAction(null, tArg);
+			}
+
+			return default(TResult);
+		}
+	}
 }
