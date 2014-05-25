e8053e897384c1a2c0875fa28ad3896d4d80dd82
ipjohnson <ipjohnson123@yahoo.com>
2014-05-25 10:12:34 -0600

    Adding WhenAncestor extension

--- Source/Grace/DependencyInjection/IFluentExportStrategyConfigurationExtensions.cs
+++ Source/Grace/DependencyInjection/IFluentExportStrategyConfigurationExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grace.DependencyInjection.Conditions;
 using Grace.DependencyInjection.Lifestyle;
 
 namespace Grace.DependencyInjection
@@ -108,6 +109,37 @@ namespace Grace.DependencyInjection
 
 			return configuration;
 		}
+		#endregion
+
+		#region When Ancestor
+
+		/// <summary>
+		/// Export strategy when being inserted into object graph that has an ancestor of type TAncestor
+		/// </summary>
+		/// <typeparam name="TAncestor">ancestor type</typeparam>
+		/// <param name="configuration">configuration object</param>
+		/// <returns>configuration object</returns>
+		public static IFluentExportStrategyConfiguration WhenAncestor<TAncestor>(this IFluentExportStrategyConfiguration configuration)
+		{
+			configuration.AndCondition(new WhenAncestor(typeof(TAncestor)));
+
+			return configuration;
+		}
+
+		/// <summary>
+		/// Export strategy when being inserted into object graph that has an ancestor of type TAncestor
+		/// </summary>
+		/// <typeparam name="TAncestor">ancestor type</typeparam>
+		/// <typeparam name="T">type being exported</typeparam>
+		/// <param name="configuration">configuration object</param>
+		/// <returns>configuration object</returns>
+		public static IFluentExportStrategyConfiguration<T> WhenAncestor<T,TAncestor>(this IFluentExportStrategyConfiguration<T> configuration)
+		{
+			configuration.AndCondition(new WhenAncestor(typeof(TAncestor)));
+
+			return configuration;
+		}
+
 		#endregion
 
 	}
