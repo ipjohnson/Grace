ffd3ead0b09eebebdb1b76587269a4e3c3a20d4a
ipjohnson <ipjohnson123@yahoo.com>
2014-05-26 18:30:23 -0600

    Adding ProcessCustomEnrichmentExpressionAttribute

--- Source/Grace/DependencyInjection/IExportTypeSetConfigurationExtensions.cs
+++ Source/Grace/DependencyInjection/IExportTypeSetConfigurationExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Grace.DependencyInjection.Attributes.Interfaces;
 using Grace.DependencyInjection.Impl;
 
 namespace Grace.DependencyInjection
@@ -10,7 +12,7 @@ namespace Grace.DependencyInjection
 	/// <summary>
 	/// extension methods for export type set configuration
 	/// </summary>
-   // ReSharper disable once InconsistentNaming
+	// ReSharper disable once InconsistentNaming
 	public static class IExportTypeSetConfigurationExtensions
 	{
 		/// <summary>
@@ -24,6 +26,69 @@ namespace Grace.DependencyInjection
 			configuration.WithInspector(new PartiallyClosedGenericPriorityAugmenter());
 
 			return configuration;
+		}
+
+		/// <summary>
+		/// Process all ICustomEnrichmentExpressionAttribute attributes
+		/// </summary>
+		/// <param name="configuration"></param>
+		/// <returns></returns>
+		public static IExportTypeSetConfiguration ProcessCustomEnrichmentExpressionAttribute(
+			this IExportTypeSetConfiguration configuration)
+		{
+			configuration.EnrichWithExpression(ProcessCustomAttributes);
+
+			return configuration;
+		}
+
+		private static IEnumerable<ICustomEnrichmentLinqExpressionProvider> ProcessCustomAttributes(Type exportType)
+		{
+			foreach (PropertyInfo runtimeProperty in exportType.GetRuntimeProperties())
+			{
+				if ((runtimeProperty.CanRead && (runtimeProperty.GetMethod.IsStatic || !runtimeProperty.GetMethod.IsPublic)) ||
+					 (runtimeProperty.CanWrite && (runtimeProperty.SetMethod.IsStatic || !runtimeProperty.SetMethod.IsPublic)))
+				{
+					continue;
+				}
+
+				foreach (Attribute customAttribute in runtimeProperty.GetCustomAttributes())
+				{
+					ICustomEnrichmentExpressionAttribute enrichmentAttribute = customAttribute as ICustomEnrichmentExpressionAttribute;
+
+					if (enrichmentAttribute != null)
+					{
+						var enrichment = enrichmentAttribute.GetProvider(exportType, runtimeProperty);
+
+						if (enrichment != null)
+						{
+							yield return enrichment;
+						}
+					}
+				}
+			}
+
+			foreach (MethodInfo runtimeMethod in exportType.GetRuntimeMethods())
+			{
+				if (runtimeMethod.IsStatic || !runtimeMethod.IsPublic)
+				{
+					continue;
+				}
+
+				foreach (Attribute customAttribute in runtimeMethod.GetCustomAttributes())
+				{
+					ICustomEnrichmentExpressionAttribute enrichmentAttribute = customAttribute as ICustomEnrichmentExpressionAttribute;
+
+					if (enrichmentAttribute != null)
+					{
+						var enrichment = enrichmentAttribute.GetProvider(exportType, runtimeMethod);
+
+						if (enrichment != null)
+						{
+							yield return enrichment;
+						}
+					}
+				}
+			}
 		}
 	}
 }
---
+++ Source/Grace/DependencyInjection/Impl/IInjectionContextProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace.DependencyInjection.Impl
+{
+	/// <summary>
+	/// Provide injection contexts for an injection scope
+	/// </summary>
+	public interface IInjectionContextProvider
+	{
+		/// <summary>
+		/// Provide an injection context
+		/// </summary>
+		/// <param name="scope">injection scope</param>
+		/// <param name="disposalScope">disposal scope</param>
+		/// <param name="disposalScopeProvider">disposal scope provider</param>
+		/// <returns>new injection context</returns>
+		IInjectionContext CreateInjectionContext(IInjectionScope scope,
+															  IDisposalScope disposalScope,
+															  IDisposalScopeProvider disposalScopeProvider);
+	}
+}
