cb0abe1ac7f8ce85199cdfe25ef45a6a40a535d4
ipjohnson <ipjohnson123@yahoo.com>
2014-07-02 21:59:55 -0600

    Adding ExportViews

--- Source/Grace.MVC5/DependencyInjection/IExportRegistrationBlockExtensions.cs
+++ Source/Grace.MVC5/DependencyInjection/IExportRegistrationBlockExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using Grace.DependencyInjection;
 
 namespace Grace.MVC.DependencyInjection
@@ -22,5 +23,19 @@ namespace Grace.MVC.DependencyInjection
 											 BasedOn(typeof(Controller)).
 											 ExternallyOwned();
 		}
+
+        /// <summary>
+        /// C# extension that export all classes that inherit from ViewPage
+        /// </summary>
+        /// <param name="registrationBlock">registration block</param>
+        /// <param name="types">list of types</param>
+        /// <returns>configuration object</returns>
+	    public static IExportTypeSetConfiguration ExportView(this IExportRegistrationBlock registrationBlock,
+	        IEnumerable<Type> types)
+        {
+            return registrationBlock.Export(types).
+                                     BasedOn(typeof(WebPage)).
+                                     ExternallyOwned();
+	    }
 	}
 }
