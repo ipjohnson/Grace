3129bb921f87ba51aafc39b8b6b3c727a2660ec2
ipjohnson <ipjohnson123@yahoo.com>
2014-07-02 21:53:29 -0600

    Adding ViewPageActivator

---
+++ Source/Grace.MVC5/DependencyInjection/ViewPageActivator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Grace.DependencyInjection;
+
+namespace Grace.MVC.DependencyInjection
+{
+    /// <summary>
+    /// Activates a new view
+    /// </summary>
+    public class ViewPageActivator : IViewPageActivator
+    {
+        private readonly IExportLocator _container;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="container"></param>
+        public ViewPageActivator(IExportLocator container)
+        {
+            _container = container;
+        }
+
+        public object Create(ControllerContext controllerContext, Type type)
+        {
+            // We use the GUID because when ASP.Net compilier re-compile it uses the same type name but different GUID
+            object returnObject = _container.Locate(type);
+
+            if (returnObject == null)
+            {
+                _container.Configure(c => c.Export(typeof(Type)));
+
+                returnObject = _container.Locate(type);
+            }
+
+            return returnObject;
+        }
+    }
+}
