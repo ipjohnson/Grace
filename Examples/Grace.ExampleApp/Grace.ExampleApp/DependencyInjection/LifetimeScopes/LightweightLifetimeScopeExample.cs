96e37db1d1ca7ef7bd71b03057021ef6f6641c6e
ipjohnson <ipjohnson123@yahoo.com>
2014-07-12 12:55:41 -0600

    Adding new examples

---
+++ Examples/Grace.ExampleApp/Grace.ExampleApp/DependencyInjection/LifetimeScopes/LightweightLifetimeScopeExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+
+namespace Grace.ExampleApp.DependencyInjection.LifetimeScopes
+{
+    public class LightweightLifetimeScopeExample : IExample<LifetimeScopesSubModules>
+    {
+        public void ExecuteExample()
+        {
+            DependencyInjectionContainer container = new DependencyInjectionContainer();
+
+            
+        }
+    }
+}
