d4e5b25934b42fcc44eef716645b1725c3e2442f
ipjohnson <ipjohnson123@yahoo.com>
2014-07-12 22:20:35 -0600

    Adding ImmutableHashTree

---
+++ Source/Grace/Data/ImmutableHashTree.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace.Data
+{
+    /// <summary>
+    /// A balanced binary search tree implemented as an AVL tree.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    public sealed class ImmutableHashTree<TKey, TValue>
+    {
+        /// <summary>
+        /// An empty <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        public static readonly ImmutableHashTree<TKey, TValue> Empty = new ImmutableHashTree<TKey, TValue>();
+
+        /// <summary>
+        /// The key of this <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        public readonly TKey Key;
+
+        /// <summary>
+        /// The value of this <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        public readonly TValue Value;
+
+        /// <summary>
+        /// The duplicates
+        /// </summary>
+        public readonly ImmutableList<KeyValuePair<TKey, TValue>> Duplicates;
+
+        /// <summary>
+        /// The hash code retrieved from the <see cref="Key"/>.
+        /// </summary>
+        public readonly int HashCode;
+
+        /// <summary>
+        /// The left node of this <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        public readonly ImmutableHashTree<TKey, TValue> Left;
+
+        /// <summary>
+        /// The right node of this <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        public readonly ImmutableHashTree<TKey, TValue> Right;
+
+        /// <summary>
+        /// The height of this node.
+        /// </summary>
+        /// <remarks>
+        /// An empty node has a height of 0 and a node without children has a height of 1.
+        /// </remarks>
+        public readonly int Height;
+
+        /// <summary>
+        /// Indicates that this <see cref="ImmutableHashTree{TKey,TValue}"/> is empty.
+        /// </summary>
+        public readonly bool IsEmpty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImmutableHashTree{TKey,TValue}"/> class
+        /// and adds a new entry in the <see cref="Duplicates"/> list.
+        /// </summary>
+        /// <param name="key">The key for this node.</param>
+        /// <param name="value">The value for this node.</param>
+        /// <param name="hashTree">The <see cref="ImmutableHashTree{TKey,TValue}"/> that contains existing duplicates.</param>
+        public ImmutableHashTree(TKey key, TValue value, ImmutableHashTree<TKey, TValue> hashTree)
+        {
+            Duplicates = hashTree.Duplicates.Add(new KeyValuePair<TKey, TValue>(key, value));
+            Key = hashTree.Key;
+            Value = hashTree.Value;
+            Height = hashTree.Height;
+            HashCode = hashTree.HashCode;
+            Left = hashTree.Left;
+            Right = hashTree.Right;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImmutableHashTree{TKey,TValue}"/> class.
+        /// </summary>
+        /// <param name="key">The key for this node.</param>
+        /// <param name="value">The value for this node.</param>
+        /// <param name="left">The left node.</param>
+        /// <param name="right">The right node.</param>
+        public ImmutableHashTree(TKey key, TValue value, ImmutableHashTree<TKey, TValue> left, ImmutableHashTree<TKey, TValue> right)
+        {
+            var balance = left.Height - right.Height;
+
+            if (balance == -2)
+            {
+                if (right.IsLeftHeavy())
+                {
+                    right = RotateRight(right);
+                }
+
+                // Rotate left
+                Key = right.Key;
+                Value = right.Value;
+                Left = new ImmutableHashTree<TKey, TValue>(key, value, left, right.Left);
+                Right = right.Right;
+            }
+            else if (balance == 2)
+            {
+                if (left.IsRightHeavy())
+                {
+                    left = RotateLeft(left);
+                }
+
+                // Rotate right
+                Key = left.Key;
+                Value = left.Value;
+                Right = new ImmutableHashTree<TKey, TValue>(key, value, left.Right, right);
+                Left = left.Left;
+            }
+            else
+            {
+                Key = key;
+                Value = value;
+                Left = left;
+                Right = right;
+            }
+
+            Height = 1 + Math.Max(Left.Height, Right.Height);
+
+            Duplicates = ImmutableList<KeyValuePair<TKey, TValue>>.Empty;
+
+            HashCode = key.GetHashCode();
+        }
+
+        private ImmutableHashTree()
+        {
+            IsEmpty = true;
+            Duplicates = ImmutableList<KeyValuePair<TKey, TValue>>.Empty;
+        }
+
+        private static ImmutableHashTree<TKey, TValue> RotateLeft(ImmutableHashTree<TKey, TValue> left)
+        {
+            return new ImmutableHashTree<TKey, TValue>(
+                left.Right.Key,
+                left.Right.Value,
+                new ImmutableHashTree<TKey, TValue>(left.Key, left.Value, left.Right.Left, left.Left),
+                left.Right.Right);
+        }
+
+        private static ImmutableHashTree<TKey, TValue> RotateRight(ImmutableHashTree<TKey, TValue> right)
+        {
+            return new ImmutableHashTree<TKey, TValue>(
+                right.Left.Key,
+                right.Left.Value,
+                right.Left.Left,
+                new ImmutableHashTree<TKey, TValue>(right.Key, right.Value, right.Left.Right, right.Right));
+        }
+
+        private bool IsLeftHeavy()
+        {
+            return Left.Height > Right.Height;
+        }
+
+        private bool IsRightHeavy()
+        {
+            return Right.Height > Left.Height;
+        }
+    }
+
+    /// <summary>
+    /// C# extensions for ImmutableHashTree
+    /// </summary>
+    public static class ImmutableHashTreeExtensions
+    {
+        /// <summary>
+        /// Convert hashtable into list of KVP
+        /// </summary>
+        /// <typeparam name="TKey">key type</typeparam>
+        /// <typeparam name="TValue">value type</typeparam>
+        /// <param name="tree">tree</param>
+        /// <returns>list of KVP</returns>
+        public static List<KeyValuePair<TKey, TValue>> ToList<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree)
+        {
+            List<KeyValuePair<TKey, TValue>> returnList = new List<KeyValuePair<TKey, TValue>>();
+
+            InternalToList(tree, returnList);
+
+            return returnList;
+        }
+
+        private static void InternalToList<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree,
+            List<KeyValuePair<TKey, TValue>> returnList)
+        {
+            returnList.Add(new KeyValuePair<TKey, TValue>(tree.Key, tree.Value));
+
+            returnList.AddRange(tree.Duplicates);
+
+            if (tree.Left != ImmutableHashTree<TKey, TValue>.Empty)
+            {
+                InternalToList(tree.Left, returnList);
+            }
+
+            if (tree.Right != ImmutableHashTree<TKey, TValue>.Empty)
+            {
+                InternalToList(tree.Right, returnList);
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of Values from the hashtree
+        /// </summary>
+        /// <typeparam name="TKey">key type</typeparam>
+        /// <typeparam name="TValue">value type</typeparam>
+        /// <param name="tree">immutable tree</param>
+        /// <returns></returns>
+        public static List<TValue> Values<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree)
+        {
+            List<TValue> returnList = new List<TValue>();
+
+            InternalValuesList(tree, returnList);
+
+            return returnList;
+        }
+
+        private static void InternalValuesList<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree,
+            List<TValue> returnList)
+        {
+            returnList.Add(tree.Value);
+
+            returnList.AddRange(tree.Duplicates.Select(x => x.Value));
+
+            if (tree.Left != ImmutableHashTree<TKey, TValue>.Empty)
+            {
+                InternalValuesList(tree.Left, returnList);
+            }
+
+            if (tree.Right != ImmutableHashTree<TKey, TValue>.Empty)
+            {
+                InternalValuesList(tree.Right, returnList);
+            }
+        }
+        
+        /// <summary>
+        /// Searches for a <typeparamref name="TValue"/> using the given <paramref name="key"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="tree">The target <see cref="ImmutableHashTree{TKey,TValue}"/>.</param>
+        /// <param name="key">The key of the <see cref="ImmutableHashTree{TKey,TValue}"/> to get.</param>
+        /// <returns>If found, the <typeparamref name="TValue"/> with the given <paramref name="key"/>, otherwise the default <typeparamref name="TValue"/>.</returns>
+        public static TValue Search<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree, TKey key)
+        {
+            int hashCode = key.GetHashCode();
+
+            while (tree.Height != 0 && tree.HashCode != hashCode)
+            {
+                tree = hashCode < tree.HashCode ? tree.Left : tree.Right;
+            }
+
+            if (!tree.IsEmpty && (ReferenceEquals(tree.Key, key) || Equals(tree.Key, key)))
+            {
+                return tree.Value;
+            }
+
+            if (tree.Duplicates.Items.Length > 0)
+            {
+                foreach (var keyValue in tree.Duplicates.Items)
+                {
+                    if (ReferenceEquals(keyValue.Key, key) || Equals(keyValue.Key, key))
+                    {
+                        return keyValue.Value;
+                    }
+                }
+            }
+
+            return default(TValue);
+        }
+
+        /// <summary>
+        /// Adds a new element to the <see cref="ImmutableHashTree{TKey,TValue}"/>.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="tree">The target <see cref="ImmutableHashTree{TKey,TValue}"/>.</param>
+        /// <param name="key">The key to be associated with the value.</param>
+        /// <param name="value">The value to be added to the tree.</param>
+        /// <returns>A new <see cref="ImmutableHashTree{TKey,TValue}"/> that contains the new key/value pair.</returns>
+        internal static ImmutableHashTree<TKey, TValue> Add<TKey, TValue>(this ImmutableHashTree<TKey, TValue> tree, TKey key, TValue value)
+        {
+            if (tree.IsEmpty)
+            {
+                return new ImmutableHashTree<TKey, TValue>(key, value, tree, tree);
+            }
+
+            int hashCode = key.GetHashCode();
+
+            if (hashCode > tree.HashCode)
+            {
+                return AddToRightBranch(tree, key, value);
+            }
+
+            if (hashCode < tree.HashCode)
+            {
+                return AddToLeftBranch(tree, key, value);
+            }
+
+            return new ImmutableHashTree<TKey, TValue>(key, value, tree);
+        }
+
+        private static ImmutableHashTree<TKey, TValue> AddToLeftBranch<TKey, TValue>(ImmutableHashTree<TKey, TValue> tree, TKey key, TValue value)
+        {
+            return new ImmutableHashTree<TKey, TValue>(tree.Key, tree.Value, tree.Left.Add(key, value), tree.Right);
+        }
+
+        private static ImmutableHashTree<TKey, TValue> AddToRightBranch<TKey, TValue>(ImmutableHashTree<TKey, TValue> tree, TKey key, TValue value)
+        {
+            return new ImmutableHashTree<TKey, TValue>(tree.Key, tree.Value, tree.Left, tree.Right.Add(key, value));
+        }
+    }
+}
---
+++ Source/Grace/Data/ImmutableList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grace.Data
+{
+
+    /// <summary>
+    /// Immutable list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class ImmutableList<T> : IEnumerable<T>
+    {
+        /// <summary>
+        /// Empty immutable list
+        /// </summary>
+        public static readonly ImmutableList<T> Empty = new ImmutableList<T>(new T[0]);
+        /// <summary>
+        /// List of items
+        /// </summary>
+        public readonly T[] Items;
+
+        /// <summary>
+        /// Cosntructor takes an IEnumerable of T
+        /// </summary>
+        /// <param name="list"></param>
+        public ImmutableList(IEnumerable<T> list)
+        {
+            Items = list.ToArray();
+        }
+
+        /// <summary>
+        /// Constructor takes an array of T
+        /// </summary>
+        /// <param name="list"></param>
+        public ImmutableList(T[] list)
+        {
+            Items = list;
+        }
+
+        /// <summary>
+        /// Get an enumerator for the list
+        /// </summary>
+        /// <returns>enumerator</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        /// <summary>
+        /// Get an enumerator for the list
+        /// </summary>
+        /// <returns>enumerator</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Creates a new immutable list with the specified new t
+        /// </summary>
+        /// <param name="newT">new instance</param>
+        /// <returns>new list</returns>
+        public ImmutableList<T> Add(T newT)
+        {
+            T[] newArray = new T[Items.Length + 1];
+
+            Array.Copy(Items, 0, newArray, 0, Items.Length);
+
+            return new ImmutableList<T>(newArray);
+        }
+
+        /// <summary>
+        /// Enumerator class for immutable list
+        /// </summary>
+        private class Enumerator : IEnumerator<T>
+        {
+            private readonly ImmutableList<T> _immutableList;
+            private int _iteration = -1;
+
+            public Enumerator(ImmutableList<T> immutableList)
+            {
+                _immutableList = immutableList;
+            }
+
+            public bool MoveNext()
+            {
+                if (_iteration + 1 > _immutableList.Items.Length)
+                {
+                    _iteration++;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                _iteration = -1;
+            }
+
+            public T Current
+            {
+                get { return _immutableList.Items[_iteration]; }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return Current; }
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+    }
+}
