fcefd3bb9ce03f23a5ca95f47ee4c2105eec39e6
ipjohnson <ipjohnson123@yahoo.com>
2016-11-23 08:00:49 -0700

    adding comments

--- src/Grace/DependencyInjection/Impl/UniqueIntId.cs
+++ src/Grace/DependencyInjection/Impl/UniqueIntId.cs
@@ -2,10 +2,17 @@
 
 namespace Grace.DependencyInjection.Impl
 {
+    /// <summary>
+    /// static class for generating unique int id within this process
+    /// </summary>
     public class UniqueIntId
     {
         private static int _currentUniqueId;
 
+        /// <summary>
+        /// Create new unique int id
+        /// </summary>
+        /// <returns></returns>
         public static int New()
         {
             return Interlocked.Increment(ref _currentUniqueId);
