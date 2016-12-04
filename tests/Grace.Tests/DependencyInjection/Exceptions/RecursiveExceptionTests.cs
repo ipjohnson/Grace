dae0ccf4b974907be10007053a25389cd4582626
ipjohnson <ipjohnson123@yahoo.com>
2016-12-04 14:01:17 -0700

    adding recursive tests

---
+++ tests/Grace.Tests/DependencyInjection/Exceptions/RecursiveExceptionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.DependencyInjection.Exceptions;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.Exceptions
+{
+    public class RecursiveExceptionTests
+    {
+        public class RecursiveClass1
+        {
+            private RecursiveClass2 _class2;
+
+            public RecursiveClass1(RecursiveClass2 class2)
+            {
+                _class2 = class2;
+            }
+        }
+
+        public class RecursiveClass2
+        {
+            private RecursiveClass1 _class1;
+
+            public RecursiveClass2(RecursiveClass1 class1)
+            {
+                _class1 = class1;
+            }
+        }
+
+        [Fact]
+        public void RecursiveException_Throws_Exception()
+        {
+            var container = new DependencyInjectionContainer();
+
+            Assert.Throws<RecursiveLocateException>(() => container.Locate<RecursiveClass1>());
+        }
+    }
+}
