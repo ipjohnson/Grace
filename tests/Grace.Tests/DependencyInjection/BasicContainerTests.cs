095949bf9a270cfb46cbcdfc2dfe913515c7fe49
ipjohnson <ipjohnson123@yahoo.com>
2016-12-04 15:14:38 -0700

    adding tests

--- tests/Grace.Tests/DependencyInjection/BasicContainerTests.cs
+++ tests/Grace.Tests/DependencyInjection/BasicContainerTests.cs
@@ -71,5 +71,48 @@ namespace Grace.Tests.DependencyInjection
 
             Assert.NotNull(instance);
         }
+
+        public interface IInheritingBasicService : IBasicService
+        {
+            
+        }
+
+        public class InheritingBasicService : BasicService, IInheritingBasicService
+        {
+            
+        }
+
+        [Fact]
+        public void DepencencyInjectionContainer_ExportAsBase_Interface()
+        {
+            var container = new DependencyInjectionContainer(c => c.ExportAsBase = true);
+
+            container.Configure(c => c.Export<InheritingBasicService>().As<IInheritingBasicService>().ImportProperty(m => m.Count).DefaultValue(5));
+
+            var instance = container.Locate<IInheritingBasicService>();
+            Assert.NotNull(instance);
+            Assert.Equal(5, instance.Count);
+
+            var basicInstance = container.Locate<IBasicService>();
+            Assert.NotNull(basicInstance);
+            Assert.Equal(5, basicInstance.Count);
+        }
+
+
+        [Fact]
+        public void DepencencyInjectionContainer_ExportAsBase_Concrete()
+        {
+            var container = new DependencyInjectionContainer(c => c.ExportAsBase = true);
+
+            container.Configure(c => c.Export<InheritingBasicService>().ImportProperty(m => m.Count).DefaultValue(5));
+
+            var instance = container.Locate<InheritingBasicService>();
+            Assert.NotNull(instance);
+            Assert.Equal(5, instance.Count);
+
+            var basicInstance = container.Locate<BasicService>();
+            Assert.NotNull(basicInstance);
+            Assert.Equal(5, basicInstance.Count);
+        }
     }
 }
---
+++ tests/Grace.Tests/DependencyInjection/StrategyCollectionContainer/StrategyCollectionContainerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.Tests.Classes.Simple;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.StrategyCollectionContainer
+{
+    public class StrategyCollectionContainerTests
+    {
+        [Fact]
+        public void StrategyCollectionContainer_Clone_Test()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c => c.Export<BasicService>().As<IBasicService>());
+
+            var cloneContainer = container.StrategyCollectionContainer.Clone();
+
+            Assert.NotNull(cloneContainer);
+            Assert.Equal(1, cloneContainer.GetAllStrategies().Count());
+        }
+    }
+}
