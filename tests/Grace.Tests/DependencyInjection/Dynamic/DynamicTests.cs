9bc6e927b91292f0cb63920a0d3c3f47cb315f33
ipjohnson <ipjohnson123@yahoo.com>
2016-12-08 19:02:09 -0700

    adding tests

--- tests/Grace.Tests/DependencyInjection/Dynamic/DynamicTests.cs
+++ tests/Grace.Tests/DependencyInjection/Dynamic/DynamicTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Grace.DependencyInjection;
 using Grace.Tests.Classes.Simple;
+using Grace.Tests.DependencyInjection.Enumerable;
 using Xunit;
 
 namespace Grace.Tests.DependencyInjection.Dynamic
@@ -44,6 +46,55 @@ namespace Grace.Tests.DependencyInjection.Dynamic
             Assert.NotNull(instance);
             Assert.NotNull(instance.Value);
             Assert.Equal(1, instance.Value.Count());
+        }
+
+
+        [Fact]
+        public void Dynamic_Parameter_Resolve_IEnumerable_With_Creator()
+        {
+            var container = new DependencyInjectionContainer(configuration => 
+                configuration.Behaviors.CustomEnumerableCreator = new EnumerableTests.ReadOnlyCreator());
+
+            container.Configure(c => c.Export(typeof(DependentService<>)).As(typeof(IDependentService<>)).WithCtorParam<object>().IsDynamic());
+
+            var instance = container.Locate<IDependentService<IEnumerable<IMultipleService>>>();
+
+            Assert.NotNull(instance);
+            Assert.NotNull(instance.Value);
+            Assert.IsType<ReadOnlyCollection<IMultipleService>>(instance.Value);
+            Assert.False(instance.Value.Any());
+
+            container.Configure(c => c.Export<MultipleService1>().As<IMultipleService>());
+
+            instance = container.Locate<IDependentService<IEnumerable<IMultipleService>>>();
+
+            Assert.NotNull(instance);
+            Assert.NotNull(instance.Value);
+            Assert.IsType<ReadOnlyCollection<IMultipleService>>(instance.Value);
+            Assert.Equal(1, instance.Value.Count());
+        }
+
+
+        [Fact]
+        public void Dynamic_Parameter_Resolve_Array()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c => c.Export(typeof(DependentService<>)).As(typeof(IDependentService<>)).WithCtorParam<object>().IsDynamic());
+
+            var instance = container.Locate<IDependentService<IMultipleService[]>>();
+
+            Assert.NotNull(instance);
+            Assert.NotNull(instance.Value);
+            Assert.False(instance.Value.Any());
+
+            container.Configure(c => c.Export<MultipleService1>().As<IMultipleService>());
+
+            instance = container.Locate<IDependentService<IMultipleService[]>>();
+
+            Assert.NotNull(instance);
+            Assert.NotNull(instance.Value);
+            Assert.Equal(1, instance.Value.Length);
         }
     }
 }
