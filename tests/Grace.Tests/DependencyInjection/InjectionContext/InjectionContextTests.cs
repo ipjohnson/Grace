d14118776f040d7d8623d05006d07e6279d3285f
ipjohnson <ipjohnson123@yahoo.com>
2016-12-10 11:33:30 -0700

    adding test

---
+++ tests/Grace.Tests/DependencyInjection/InjectionContext/InjectionContextTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.InjectionContext
+{
+    public class InjectionContextTests
+    {
+        [Fact]
+        public void InjectionContext_Keys_Empty()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            Assert.Equal(0, context.Keys.Count());
+        }
+
+        [Fact]
+        public void InjectionContext_Keys()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData("Hello", "World");
+
+            var keys = context.Keys.ToArray();
+
+            Assert.Equal(1, keys.Length);
+            Assert.Equal("Hello", keys[0]);
+        }
+
+        [Fact]
+        public void InjectionContext_Value()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData("Hello", "World");
+
+            var keys = context.Values.ToArray();
+
+            Assert.Equal(1, keys.Length);
+            Assert.Equal("World", keys[0]);
+        }
+
+        [Fact]
+        public void InjectionContext_Value_Empty()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            Assert.Equal(0, context.Values.Count());
+        }
+
+        [Fact]
+        public void InjectionContext_KVP()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData("Hello", "World");
+
+            var keys = context.KeyValuePairs.ToArray();
+
+            Assert.Equal(1, keys.Length);
+            Assert.Equal("Hello", keys[0].Key);
+            Assert.Equal("World", keys[0].Value);
+        }
+
+
+        [Fact]
+        public void InjectionContext_KVP_Empty()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            Assert.Equal(0, context.KeyValuePairs.Count());
+        }
+
+        [Fact]
+        public void InjectionContext_SetValue()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData(5, "Hello");
+
+            Assert.Equal("Hello", context.GetExtraData(5));
+        }
+
+
+        [Fact]
+        public void InjectionContext_SetValue_Override()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData(5, "Hello");
+
+            Assert.Equal("Hello", context.GetExtraData(5));
+
+            context.SetExtraData(5, "World");
+
+            Assert.Equal("World", context.GetExtraData(5));
+        }
+        
+        [Fact]
+        public void InjectionContext_SetValue_Dont_Override()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData(5, "Hello");
+
+            Assert.Equal("Hello", context.GetExtraData(5));
+
+            context.SetExtraData(5, "World", replaceIfExists: false);
+
+            Assert.Equal("Hello", context.GetExtraData(5));
+        }
+
+        [Fact]
+        public void InjectionContext_GetValueByType()
+        {
+            var context = new Grace.DependencyInjection.Impl.InjectionContext(null);
+
+            context.SetExtraData(5, "Hello");
+
+            Assert.Equal("Hello",context.GetValueByType(typeof(string)));
+        }
+    }
+}
---
+++ tests/Grace.Tests/DependencyInjection/Registration/ExportRegistrationBlockTests.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.DependencyInjection.Impl;
+using Grace.Tests.Classes.Simple;
+using SimpleFixture.NSubstitute;
+using SimpleFixture.xUnit;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.Registration
+{
+    [SubFixtureInitialize]
+    public class ExportRegistrationBlockTests
+    {
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Null(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.Export((Type) null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Enuemrable_Null(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.Export((IEnumerable<Type>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Instance(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportInstance((IBasicService)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Instance_Func(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportInstance((Func<IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Instance_FuncScope(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportInstance((Func<IExportLocatorScope, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Instance_FuncScope_StaticContext(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportInstance((Func<IExportLocatorScope,StaticInjectionContext, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Instance_FuncScope_StaticContext_InjectionContext(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportInstance((Func<IExportLocatorScope, StaticInjectionContext,IInjectionContext, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_0_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_1_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IExportLocatorScope, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_2_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IExportLocatorScope, StaticInjectionContext, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_3_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IExportLocatorScope, StaticInjectionContext, IInjectionContext, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_4_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IExportLocatorScope, StaticInjectionContext, IInjectionContext, IMultipleService, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_Export_Factory_5_Arg(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportFactory((Func<IExportLocatorScope, StaticInjectionContext, IInjectionContext,IMultipleService,ISimpleObject, IBasicService>)null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_ExportWrapper(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportWrapper(null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_ExportDecorator(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.ExportDecorator(null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_AddInspector(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.AddInspector(null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_AddInjectionValueProvider(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.AddInjectionValueProvider(null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_AddMissing(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.AddMissingExportStrategyProvider(null));
+        }
+
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_AddActivationStrategy(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.AddActivationStrategy(null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ExportRegistrationBlock_AddExportStrategyProvider(ExportRegistrationBlock block)
+        {
+            Assert.Throws<ArgumentNullException>(() => block.AddExportStrategyProvider(null));
+        }
+    }
+}
