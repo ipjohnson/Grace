56c7a69244c45b28e4a5e45717578df7b6a622b6
ipjohnson <ipjohnson123@yahoo.com>
2016-12-11 08:52:21 -0700

    adding tests

--- tests/Grace.Tests/DependencyInjection/AddOns/InjectionValueProviderTests.cs
+++ tests/Grace.Tests/DependencyInjection/AddOns/InjectionValueProviderTests.cs
@@ -1,11 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.Tests.Classes.Simple;
+using Xunit;
 
 namespace Grace.Tests.DependencyInjection.AddOns
 {
     public class InjectionValueProviderTests
     {
+        public class ValueProvider : IInjectionValueProvider
+        {
+            public IBasicService BasicService { get; set; }
+
+            public IActivationExpressionResult GetExpressionResult(IInjectionScope scope, IActivationExpressionRequest request)
+            {
+                if (request.ActivationType == typeof(IBasicService))
+                {
+                    return request.Services.Compiler.CreateNewResult(request, Expression.Constant(BasicService));
+                }
+
+                return null;
+            }
+        }
+
+        [Fact]
+        public void InjectionValueProvider_Test()
+        {
+            var container = new DependencyInjectionContainer();
+
+            var basicService = new BasicService { Count = 15 };
+
+            container.Configure(c =>
+            {
+                c.AddInjectionValueProvider(new ValueProvider { BasicService = basicService });
+                c.Export(typeof(DependentService<>)).As(typeof(IDependentService<>));
+                c.Export<MultipleService1>().As<IMultipleService>();
+            });
+
+            var valueProvderInstance = container.Locate<IDependentService<IBasicService>>();
+
+            Assert.NotNull(valueProvderInstance);
+            Assert.NotNull(valueProvderInstance.Value);
+            Assert.Equal(15, valueProvderInstance.Value.Count);
+
+            var nonValueProviderInstance = container.Locate<IDependentService<IMultipleService>>();
+
+            Assert.NotNull(valueProvderInstance);
+            Assert.NotNull(nonValueProviderInstance.Value);
+        }
     }
 }
--- tests/Grace.Tests/DependencyInjection/Dynamic/DynamicTests.cs
+++ tests/Grace.Tests/DependencyInjection/Dynamic/DynamicTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using Grace.DependencyInjection;
+using Grace.DependencyInjection.Exceptions;
 using Grace.Tests.Classes.Simple;
 using Grace.Tests.DependencyInjection.Enumerable;
 using Xunit;
@@ -10,6 +11,59 @@ namespace Grace.Tests.DependencyInjection.Dynamic
 {
     public class DynamicTests
     {
+        [Fact]
+        public void Dynamic_Constructor_Parameter_Missing_Throws()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c => c.Export(typeof(DependentService<>)).As(typeof(IDependentService<>)).WithCtorParam<object>().IsDynamic());
+
+            using (var childScope = container.CreateChildScope())
+            {
+                Assert.Throws<LocateException>(() => childScope.Locate<IDependentService<IBasicService>>());
+
+                childScope.Configure(c => c.Export<BasicService>().As<IBasicService>());
+
+                var instance = childScope.Locate<IDependentService<IBasicService>>();
+
+                Assert.NotNull(instance);
+                Assert.NotNull(instance.Value);
+            }
+        }
+
+        [Fact]
+        public void Dynamic_Constructor_Parameter_Returns_Default()
+        {
+            var container = new DependencyInjectionContainer();
+
+            var basicService = new BasicService { Count = 10 };
+
+            container.Configure(
+                c =>
+                    c.Export<DependentService<IBasicService>>()
+                        .As<IDependentService<IBasicService>>()
+                        .WithCtorParam<IBasicService>()
+                        .IsDynamic()
+                        .DefaultValue(basicService));
+
+            using (var childScope = container.CreateChildScope())
+            {
+                var instance = childScope.Locate<IDependentService<IBasicService>>();
+
+                Assert.NotNull(instance);
+                Assert.NotNull(instance.Value);
+                Assert.Equal(10, instance.Value.Count);
+
+                childScope.Configure(c => c.Export<BasicService>().As<IBasicService>().ImportProperty(b => b.Count).DefaultValue(20));
+
+                instance = childScope.Locate<IDependentService<IBasicService>>();
+
+                Assert.NotNull(instance);
+                Assert.NotNull(instance.Value);
+                Assert.Equal(20, instance.Value.Count);
+            }
+        }
+        
         [Fact]
         public void Dynamic_Constructor_Parameter_Resolve_From_Child_Scope()
         {
@@ -52,7 +106,7 @@ namespace Grace.Tests.DependencyInjection.Dynamic
         [Fact]
         public void Dynamic_Parameter_Resolve_IEnumerable_With_Creator()
         {
-            var container = new DependencyInjectionContainer(configuration => 
+            var container = new DependencyInjectionContainer(configuration =>
                 configuration.Behaviors.CustomEnumerableCreator = new EnumerableTests.ReadOnlyCreator());
 
             container.Configure(c => c.Export(typeof(DependentService<>)).As(typeof(IDependentService<>)).WithCtorParam<object>().IsDynamic());
