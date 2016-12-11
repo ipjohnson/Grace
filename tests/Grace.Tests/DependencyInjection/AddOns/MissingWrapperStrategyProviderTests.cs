657657c3659b63ee7d475dd37e0ef9d8ea2ec83b
ipjohnson <ipjohnson123@yahoo.com>
2016-12-11 14:54:31 -0700

    Adding tests

---
+++ tests/Grace.Tests/DependencyInjection/AddOns/MissingWrapperStrategyProviderTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Grace.DependencyInjection.Impl.CompiledStrategies;
+
+namespace Grace.Tests.DependencyInjection.AddOns
+{
+    public class MissingWrapperStrategyProviderTests
+    {
+        public class WrapperClass<T>
+        {
+            public WrapperClass(T wrapped)
+            {
+                Wrapped = wrapped;
+            }
+
+            public T Wrapped { get; }
+        }
+
+        public class WrapperProvider : IMissingExportStrategyProvider
+        {
+            public IEnumerable<IActivationStrategy> ProvideExports(IInjectionScope scope, IActivationExpressionRequest request)
+            {
+                if (request.ActivationType.IsConstructedGenericType &&
+                    request.ActivationType.GetGenericTypeDefinition() == typeof(WrapperClass<>))
+                {
+                    yield break;
+                }
+            }
+        }
+    }
+}
