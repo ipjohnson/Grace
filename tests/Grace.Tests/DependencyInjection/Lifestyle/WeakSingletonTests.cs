d251af16091f876120bf5f9623571914635bfbc3
ipjohnson <ipjohnson123@yahoo.com>
2016-12-18 11:52:51 -0700

    adding tests

---
+++ tests/Grace.Tests/DependencyInjection/Lifestyle/WeakSingletonTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.Lifestyle
+{
+    public class WeakSingletonTests
+    {
+        public class UniqueInstanceClass
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+
+        [Fact]
+        public void WeakSingleton_Return_Same_Instance()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c => c.Export<UniqueInstanceClass>().Lifestyle.WeakSingleton());
+
+            Guid instanceGuid = TestWeakLifestyle(container);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Guid instance2Guid = TestWeakLifestyle(container);
+
+            Assert.NotEqual(instanceGuid, instance2Guid);
+        }
+
+        private static Guid TestWeakLifestyle(DependencyInjectionContainer container)
+        {
+            var instance = container.Locate<UniqueInstanceClass>();
+
+            Assert.Same(instance, container.Locate<UniqueInstanceClass>());
+
+            return instance.Id;
+        }
+    }
+}
