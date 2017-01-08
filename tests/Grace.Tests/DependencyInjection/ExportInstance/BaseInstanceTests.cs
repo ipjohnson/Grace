8372a5c07f32c9b9d0ab22b36c9b55bdc45f2357
ipjohnson <ipjohnson123@yahoo.com>
2017-01-08 09:17:47 -0700

    adding class

---
+++ tests/Grace.Tests/DependencyInjection/ExportInstance/BaseInstanceTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grace.Tests.DependencyInjection.ExportInstance
+{
+    public class BaseInstanceTests
+    {
+    }
+}
