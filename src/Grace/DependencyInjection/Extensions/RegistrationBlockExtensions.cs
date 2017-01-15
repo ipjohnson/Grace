ed4808cf839d314f3f73dd27f25b630843dd70db
ipjohnson <ipjohnson123@yahoo.com>
2017-01-15 13:14:09 -0700

    adding extension to export function with no extras

---
+++ src/Grace/DependencyInjection/Extensions/RegistrationBlockExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grace.DependencyInjection.Impl.CompiledStrategies;
+
+namespace Grace.DependencyInjection.Extensions
+{
+    /// <summary>
+    /// C# registration block extensions
+    /// </summary>
+    public static class RegistrationBlockExtensions
+    {
+        /// <summary>
+        /// Directly export a function with no decoratory, no testing for null, no lifestyle. 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="block"></param>
+        /// <param name="func"></param>
+        public static void ExportFunc<T>(this IExportRegistrationBlock block, Func<IExportLocatorScope, T> func)
+        {
+            block.AddActivationStrategy(new SimpleFuncExportStrategy<T>(func, block.OwningScope));
+        }
+    }
+}
---
+++ src/Grace/DependencyInjection/Impl/CompiledStrategies/SimpleFuncExportStrategy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using Grace.Data.Immutable;
+using Grace.DependencyInjection.Lifestyle;
+
+namespace Grace.DependencyInjection.Impl.CompiledStrategies
+{
+    /// <summary>
+    /// This strategy doesn't honor lifestyle or decorators, it's intended to be very simple
+    /// </summary>
+    public class SimpleFuncExportStrategy<T> : ConfigurableActivationStrategy, ICompiledExportStrategy
+    {
+        private readonly ActivationStrategyDelegate _delegate;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="injectionScope">owning injection scope</param>
+        public SimpleFuncExportStrategy(Func<IExportLocatorScope, T> func, IInjectionScope injectionScope) : base(typeof(T), injectionScope)
+        {
+            _delegate = (scope, disposalScope, context) => func(injectionScope);
+        }
+
+        /// <summary>
+        /// Type of activation strategy
+        /// </summary>
+        public override ActivationStrategyType StrategyType { get; } = ActivationStrategyType.ExportStrategy;
+
+        /// <summary>
+        /// Get an activation expression for this strategy
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="request"></param>
+        /// <param name="lifestyle"></param>
+        /// <returns></returns>
+        public IActivationExpressionResult GetDecoratorActivationExpression(IInjectionScope scope, IActivationExpressionRequest request,
+            ICompiledLifestyle lifestyle)
+        {
+            throw new NotSupportedException("THis strategy type does not support decorators");
+        }
+
+        /// <summary>
+        /// Get an activation strategy for this delegate
+        /// </summary>
+        /// <param name="scope">injection scope</param>
+        /// <param name="compiler"></param>
+        /// <param name="activationType">activation type</param>
+        /// <returns>activation delegate</returns>
+        public ActivationStrategyDelegate GetActivationStrategyDelegate(IInjectionScope scope, IActivationStrategyCompiler compiler,
+            Type activationType)
+        {
+            return _delegate;
+        }
+
+        /// <summary>
+        /// Get an activation expression for this strategy
+        /// </summary>
+        /// <param name="scope"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public IActivationExpressionResult GetActivationExpression(IInjectionScope scope, IActivationExpressionRequest request)
+        {
+            var expression = Expression.Call(Expression.Constant(_delegate.Target), _delegate.GetMethodInfo(),
+                request.Constants.ScopeParameter);
+
+            return request.Services.Compiler.CreateNewResult(request, expression);
+        }
+
+        /// <summary>
+        /// Add a secondary strategy for this export strategy
+        /// </summary>
+        /// <param name="secondaryStrategy">new secondary strategy</param>
+        public void AddSecondaryStrategy(ICompiledExportStrategy secondaryStrategy)
+        {
+            throw new NotSupportedException("Secondary strategies are not supported on this type");
+        }
+
+        /// <summary>
+        /// Provide secondary strategies such as exporting property or method
+        /// </summary>
+        /// <returns>export strategies</returns>
+        public IEnumerable<ICompiledExportStrategy> SecondaryStrategies()
+        {
+            return ImmutableLinkedList<ICompiledExportStrategy>.Empty;
+        }
+    }
+}
