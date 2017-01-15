8a1d28bd27c7f3831d3dc7fa50506c4b6d145010
ipjohnson <ipjohnson123@yahoo.com>
2017-01-15 12:53:45 -0700

    adding test for AsyncLazy pattern

---
+++ tests/Grace.Tests/DependencyInjection/Wrappers/AsyncLazyTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Grace.DependencyInjection;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.Wrappers
+{
+    public class AsyncLazy<T>
+    {
+        private readonly Lazy<T> _lazy;
+        private TaskCompletionSource<T> _completionSource;
+
+        public AsyncLazy(Lazy<T> lazy)
+        {
+            _lazy = lazy;
+        }
+
+        public Task<T> Value
+        {
+            get
+            {
+                if (_completionSource == null && 
+                    Interlocked.CompareExchange(ref _completionSource, new TaskCompletionSource<T>(), null) == null)
+                {
+                    Task.Run(() =>
+                    {
+                        try
+                        {
+                            _completionSource.SetResult(_lazy.Value);
+                        }
+                        catch (Exception exp)
+                        {
+                            _completionSource.SetException(exp);
+                        }
+                    });
+                }
+
+                return _completionSource.Task;
+            }
+        }
+    }
+
+    public class AsyncLazyTests
+    {
+        [Fact]
+        public void AsyncLazy_Resolve()
+        {
+            var task = ResolveAsync();
+
+            task.Wait(10000);
+        }
+
+        public class DelayedResolveClass
+        {
+            public DelayedResolveClass()
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        private async Task ResolveAsync()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c =>
+            {
+                c.Export<DelayedResolveClass>();
+                c.ExportWrapper(typeof(AsyncLazy<>));
+            });
+
+            var lazy = container.Locate<AsyncLazy<DelayedResolveClass>>();
+
+            var startTime = DateTime.Now;
+
+            var value = await lazy.Value;
+
+            var endTime = DateTime.Now;
+
+            Assert.NotNull(value);
+            Assert.True((endTime - startTime).TotalSeconds > 1);
+        }
+    }
+}
