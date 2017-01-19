25803f3ca5f929e343af4bf6c94435f198e5c4ac
ipjohnson <ipjohnson123@yahoo.com>
2017-01-18 21:50:08 -0700

    adding test

--- tests/Grace.Tests/DependencyInjection/Wrappers/LazyWrapperTests.cs
+++ tests/Grace.Tests/DependencyInjection/Wrappers/LazyWrapperTests.cs
@@ -3,12 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grace.DependencyInjection;
+using Grace.Tests.Classes.Simple;
 using Xunit;
 
 namespace Grace.Tests.DependencyInjection.Wrappers
 {
     public class LazyWrapperTests
     {
+        [Fact]
+        public void Lazy_Array()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c =>
+            {
+                c.Export<MultipleService1>().As<IMultipleService>();
+                c.Export<MultipleService2>().As<IMultipleService>();
+                c.Export<MultipleService3>().As<IMultipleService>();
+                c.Export<MultipleService4>().As<IMultipleService>();
+                c.Export<MultipleService5>().As<IMultipleService>();
+            });
+
+            var array = container.Locate<Lazy<IMultipleService>[]>();
+
+            Assert.Equal(5, array.Length);
+
+            Assert.IsType<MultipleService1>(array[0].Value);
+            Assert.IsType<MultipleService2>(array[1].Value);
+            Assert.IsType<MultipleService3>(array[2].Value);
+            Assert.IsType<MultipleService4>(array[3].Value);
+            Assert.IsType<MultipleService5>(array[4].Value);
+        }
+
+
         public class LazyType1
         {
             private Lazy<LazyType2> _lazy;
