f29afa245e7da5b3766683b8499f06cc0c96edc0
ipjohnson <ipjohnson123@yahoo.com>
2017-01-19 17:29:40 -0700

    adding code for exporting by type

--- src/Grace/DependencyInjection/Attributes/ExportAttribute.cs
+++ src/Grace/DependencyInjection/Attributes/ExportAttribute.cs
@@ -5,43 +5,43 @@ using Grace.DependencyInjection.Attributes.Interfaces;
 
 namespace Grace.DependencyInjection.Attributes
 {
-	/// <summary>
-	/// This attribute is used to mark a type for export. 
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = true)]
-	public class ExportAttribute : Attribute, IExportAttribute
-	{
-		/// <summary>
-		/// Export by type rather than by name
-		/// </summary>
-		/// <param name="exportTypes"></param>
-		public ExportAttribute(params Type[] exportTypes)
-		{
-		    if (exportTypes != null && exportTypes.Length > 0)
-		    {
-		        ExportTypes = exportTypes;
-		    }
-		}
-        
+    /// <summary>
+    /// This attribute is used to mark a type for export. 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Method, AllowMultiple = true)]
+    public class ExportAttribute : Attribute, IExportAttribute
+    {
+        /// <summary>
+        /// Export by type rather than by name
+        /// </summary>
+        /// <param name="exportTypes"></param>
+        public ExportAttribute(params Type[] exportTypes)
+        {
+            if (exportTypes != null && exportTypes.Length > 0)
+            {
+                ExportTypes = exportTypes;
+            }
+        }
 
-		/// <summary>
-		/// List of export types
-		/// </summary>
-		protected Type[] ExportTypes { get; set; }
-        
-		/// <summary>
-		/// Provide a list of types to export as
-		/// </summary>
-		/// <param name="attributedType"></param>
-		/// <returns></returns>
-		public IEnumerable<Type> ProvideExportTypes(Type attributedType)
-		{
-			if (ExportTypes != null)
-			{
-				return ExportTypes;
-			}
 
-			return ImmutableArray<Type>.Empty;
-		}
-	}
+        /// <summary>
+        /// List of export types
+        /// </summary>
+        protected Type[] ExportTypes { get; set; }
+
+        /// <summary>
+        /// Provide a list of types to export as
+        /// </summary>
+        /// <param name="attributedType"></param>
+        /// <returns></returns>
+        public IEnumerable<Type> ProvideExportTypes(Type attributedType)
+        {
+            if (ExportTypes != null)
+            {
+                return ExportTypes;
+            }
+
+            return new[] { attributedType };
+        }
+    }
 }
