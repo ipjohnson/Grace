0c8ce94ac79b0bd4c25b1b1c5be9ccaf3508cf4b
ipjohnson <ipjohnson123@yahoo.com>
2017-03-26 21:54:20 -0600

    merging list and entry tofether

--- src/Grace/DependencyInjection/Impl/DisposalScope.cs
+++ src/Grace/DependencyInjection/Impl/DisposalScope.cs
@@ -10,29 +10,37 @@ namespace Grace.DependencyInjection.Impl
     /// </summary>
     public class DisposalScope : IDisposalScope
     {
-        private struct DisposableEntry
+        private class DisposableEntry
         {
             public IDisposable DisposableItem;
 
             public Action DisposalAction;
+
+            public DisposableEntry Next;
+
+            public static readonly DisposableEntry Empty = new DisposableEntry();
         }
 
-        private ImmutableLinkedList<DisposableEntry> _disposable =
-            ImmutableLinkedList<DisposableEntry>.Empty;
-
+        //private ImmutableLinkedList<DisposableEntry> _disposable =
+        //    ImmutableLinkedList<DisposableEntry>.Empty;
+        private DisposableEntry _entry = DisposableEntry.Empty;
+        
         /// <summary>
         /// Dispose of scope
         /// </summary>
         public void Dispose()
         {
-            var disposable = Interlocked.Exchange(ref _disposable, ImmutableLinkedList<DisposableEntry>.Empty);
+            var entry = Interlocked.Exchange(ref _entry, null);
 
-            while (disposable.Count != 0)
+            if (entry != null)
             {
-                disposable.Value.DisposalAction?.Invoke();
-                disposable.Value.DisposableItem.Dispose();
+                while (entry != null)
+                {
+                    entry.DisposalAction?.Invoke();
+                    entry.DisposableItem.Dispose();
 
-                disposable = disposable.Next;
+                    entry = entry.Next;
+                }
             }
         }
 
@@ -42,19 +50,51 @@ namespace Grace.DependencyInjection.Impl
         /// <param name="disposable">disposable object to track</param>
         /// <param name="cleanupDelegate">logic that will be run directly before the object is disposed</param>
         public T AddDisposable<T>(T disposable, Action<T> cleanupDelegate = null) where T : IDisposable
-        {
+        { 
+            DisposableEntry entry;
+
             if (cleanupDelegate == null)
             {
-                ImmutableLinkedList.ThreadSafeAdd(ref _disposable,
-                    new DisposableEntry { DisposableItem = disposable });
+                entry = new DisposableEntry {DisposableItem = disposable, Next = _entry};
+            }
+            else
+            {
+                entry = new DisposableEntry
+                {
+                    DisposableItem = disposable,
+                    DisposalAction = () => cleanupDelegate(disposable),
+                    Next = _entry
+                };
+            }
 
+            var current = _entry;
+
+            if (Interlocked.CompareExchange(ref _entry, entry, current) == current)
+            {
                 return disposable;
             }
 
-            ImmutableLinkedList.ThreadSafeAdd(ref _disposable,
-                new DisposableEntry { DisposableItem = disposable, DisposalAction = () => cleanupDelegate(disposable) });
+            SwapWaitAdd(entry);
 
             return disposable;
+        }
+
+        private void SwapWaitAdd(DisposableEntry entry)
+        {
+            var spinWait = new SpinWait();
+
+            spinWait.SpinOnce();
+
+            var current = _entry;
+
+            while (Interlocked.CompareExchange(ref _entry, entry, current) != current)
+            {
+                spinWait.SpinOnce();
+
+                current = _entry;
+
+                entry.Next = _entry;
+            }
         }
     }
 }
