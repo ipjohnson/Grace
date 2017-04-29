d6246fca536cdca34b86eee800e65998fda97064
ipjohnson <ipjohnson123@yahoo.com>
2017-04-29 07:55:54 -0600

    adding more tests

--- tests/Grace.Tests/DependencyInjection/Factory/ExportFuncTests.cs
+++ tests/Grace.Tests/DependencyInjection/Factory/ExportFuncTests.cs
@@ -1,10 +1,15 @@
-﻿using Grace.DependencyInjection;
+﻿using System;
+using Grace.DependencyInjection;
 using Grace.DependencyInjection.Extensions;
+using Grace.DependencyInjection.Impl.CompiledStrategies;
 using Grace.Tests.Classes.Simple;
+using SimpleFixture.NSubstitute;
+using SimpleFixture.xUnit;
 using Xunit;
 
 namespace Grace.Tests.DependencyInjection.Factory
 {
+    [SubFixtureInitialize]
     public class ExportFuncTests
     {
         [Fact]
@@ -23,6 +28,39 @@ namespace Grace.Tests.DependencyInjection.Factory
             Assert.NotNull(instance);
             Assert.NotNull(instance.Value);
             Assert.IsType<BasicService>(instance.Value);
+        }
+
+        [Theory]
+        [AutoData]
+        public void SimpleFuncExportStrategy_GetDecoratorExpression_Throws(
+            SimpleFuncExportStrategy<IBasicService> strategy,
+            IInjectionScope scope, 
+            IActivationExpressionRequest request)
+        {
+            Assert.Throws<NotSupportedException>(() => strategy.GetDecoratorActivationExpression(scope, request, null));
+        }
+
+        [Theory]
+        [AutoData]
+        public void SimpleFuncExportStrategy_AddSecondaryStrategy_Throws(
+            SimpleFuncExportStrategy<IBasicService> strategy)
+        {
+            Assert.Throws<NotSupportedException>(() => strategy.AddSecondaryStrategy(null));
+        }
+
+        [Fact]
+        public void SimpleFuncExportStrategy_GetStrategyActivationDelegate()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c =>
+            {
+                c.ExportFunc<IBasicService>(scope => new BasicService());
+            });
+
+            var instance = container.Locate<IBasicService>();
+
+            Assert.NotNull(instance);
         }
     }
 }
