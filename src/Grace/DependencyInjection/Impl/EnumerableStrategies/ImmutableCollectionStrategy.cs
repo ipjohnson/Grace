fa175a7bffaf10cfe5dfbdd6b34eb9d0e487a9bf
ipjohnson <ipjohnson123@yahoo.com>
2017-08-12 06:59:46 -0600

    add generic base strategy issue #105

--- src/Grace/DependencyInjection/Impl/EnumerableStrategies/ImmutableCollectionStrategy.cs
+++ src/Grace/DependencyInjection/Impl/EnumerableStrategies/ImmutableCollectionStrategy.cs
@@ -39,18 +39,6 @@ namespace Grace.DependencyInjection.Impl.EnumerableStrategies
                 });
         }
 
-        /// <summary>
-        /// Get an activation expression for this strategy
-        /// </summary>
-        /// <param name="scope"></param>
-        /// <param name="request"></param>
-        /// <param name="lifestyle"></param>
-        /// <returns></returns>
-        public override IActivationExpressionResult GetDecoratorActivationExpression(IInjectionScope scope,
-            IActivationExpressionRequest request, ICompiledLifestyle lifestyle)
-        {
-            throw new NotSupportedException("Decorators on collection is not supported at this time");
-        }
 
         /// <summary>
         /// Get an activation expression for this strategy
