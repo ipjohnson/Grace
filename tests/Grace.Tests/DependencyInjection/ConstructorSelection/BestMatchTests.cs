03749c7c1700dfc44d6516017e249789aed4b6a8
ipjohnson <ipjohnson123@yahoo.com>
2017-08-18 06:14:18 -0600

    taking known values into consideration for issue #109

--- tests/Grace.Tests/DependencyInjection/ConstructorSelection/BestMatchTests.cs
+++ tests/Grace.Tests/DependencyInjection/ConstructorSelection/BestMatchTests.cs
@@ -1,4 +1,5 @@
-﻿using Grace.DependencyInjection;
+﻿using System;
+using Grace.DependencyInjection;
 using Grace.Tests.Classes.Simple;
 using Xunit;
 
@@ -56,5 +57,50 @@ namespace Grace.Tests.DependencyInjection.ConstructorSelection
             Assert.NotNull(service.BasicService);
             Assert.NotNull(service.ConstructorImportService);
         }
+
+
+        public interface IHaveMultipleConstructors
+        {
+            int NumProp { get; }
+
+            string StringProp { get; }
+        }
+
+
+        public class HaveMultipleConstructors : IHaveMultipleConstructors
+        {
+            public HaveMultipleConstructors()
+            {
+
+            }
+
+            public HaveMultipleConstructors(string stringValue, int intValue)
+            {
+
+                NumProp = intValue;
+
+                StringProp = stringValue;
+            }
+
+            public int NumProp { get; private set; }
+
+            public string StringProp { get; private set; }
+        }
+
+        [Fact]
+        public void Container_Match_Best_Constructor_Func()
+        {
+            DependencyInjectionContainer di = new DependencyInjectionContainer();
+
+            di.Configure(config =>
+            {
+                config.Export<HaveMultipleConstructors>().As<IHaveMultipleConstructors>();
+            });
+
+            var myFunc = di.Locate<Func<string, int, IHaveMultipleConstructors>>();
+
+            var functioned = myFunc("funcString", 667);
+        }
+        
     }
 }
