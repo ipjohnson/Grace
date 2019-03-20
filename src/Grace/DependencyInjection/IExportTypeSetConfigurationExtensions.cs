431300547f515df4ebf56b969cbbee37404949a7
Ian Johnson <ipjohnson123@yahoo.com>
2019-03-20 16:53:40 -0400

    Adding extension for issue #205

--- src/Grace/DependencyInjection/IExportTypeSetConfigurationExtensions.cs
+++ src/Grace/DependencyInjection/IExportTypeSetConfigurationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Grace.DependencyInjection.Impl;
+﻿using System;
+using System.Reflection;
+using Grace.DependencyInjection.Impl;
 
 namespace Grace.DependencyInjection
 {
@@ -16,6 +18,35 @@ namespace Grace.DependencyInjection
             this IExportTypeSetConfiguration configuration)
         {
             configuration.WithInspector(new PartiallyClosedGenericPriorityAugmenter());
+
+            return configuration;
+        }
+
+        /// <summary>
+        /// Export a type set by a keyed interface
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="configuration"></param>
+        /// <param name="keyFunc"></param>
+        /// <returns></returns>
+        public static IExportTypeSetConfiguration ByKeyed<T>(this IExportTypeSetConfiguration configuration, Func<Type, object> keyFunc)
+        {
+            configuration.ByKeyedTypes(type =>
+            {
+                if (typeof(T).GetTypeInfo().IsAssignableFrom(type.GetTypeInfo()))
+                {
+                    var key = keyFunc(type);
+
+                    if (key != null)
+                    {
+                        return new[] { new Tuple<Type, object>(type, key) };
+                    }
+
+                    return null;
+                }
+
+                return null;
+            });
 
             return configuration;
         }
