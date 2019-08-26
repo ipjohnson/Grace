9be9a879e661148dacd67ea1a0cbaff79f76fa5e
Ian Johnson <ipjohnson123@yahoo.com>
2019-08-26 07:15:03 -0400

    adding convention based test for issue #232

---
+++ tests/Grace.Tests/Classes/Simple/ConventionBasedService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grace.Tests.Classes.Simple
+{
+    public interface IConventionBasedService
+    {
+        IMultipleService ServiceA { get; }
+
+        IMultipleService ServiceB { get; }
+
+    }
+
+    public class ConventionBasedService : IConventionBasedService
+    {
+        public ConventionBasedService(IMultipleService serviceA, IMultipleService serviceB)
+        {
+            ServiceA = serviceA;
+            ServiceB = serviceB;
+        }
+
+        public IMultipleService ServiceA { get; }
+
+        public IMultipleService ServiceB { get; }
+    }
+}
---
+++ tests/Grace.Tests/DependencyInjection/Registration/ConventionBasedRegistrationTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Grace.DependencyInjection;
+using Grace.DependencyInjection.Impl;
+using Grace.Tests.Classes.Simple;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.Registration
+{
+    public class ConventionBasedRegistrationTest
+    {
+        [Fact]
+        public void ConventionBasedNameTest()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c =>
+            {
+                c.AddInjectionValueProvider(new ConventionBasedValueProvider());
+                c.Export<MultipleService1>().AsKeyed<IMultipleService>("serviceA");
+                c.Export<MultipleService2>().AsKeyed<IMultipleService>("serviceB");
+                c.Export(typeof(ConventionBasedRegistrationTest).Assembly.ExportedTypes).ByInterfaces();
+            });
+
+            var instance = container.Locate<IConventionBasedService>();
+
+            Assert.NotNull(instance);
+            Assert.IsType<MultipleService1>(instance.ServiceA);
+            Assert.IsType<MultipleService2>(instance.ServiceB);
+        }
+
+        /// <summary>
+        /// This class is used to add a key lookup for IMultipleService dependency
+        /// </summary>
+        public class ConventionBasedValueProvider : IInjectionValueProvider
+        {
+            public bool CanLocate(IInjectionScope scope, IActivationExpressionRequest request)
+            {
+                return false;
+            }
+
+            public IActivationExpressionResult GetExpressionResult(IInjectionScope scope, IActivationExpressionRequest request)
+            {
+                // test if it's the interface we're looking for and it's for a parameter
+                if (request.ActivationType == typeof(IMultipleService) && 
+                    request.Info is ParameterInfo memberInfo)
+                {
+                    // set the locate key automatically to be the parameter name
+                    request.SetLocateKey(memberInfo.Name);
+                }
+
+                return null;
+            }
+        }
+    }
+}
