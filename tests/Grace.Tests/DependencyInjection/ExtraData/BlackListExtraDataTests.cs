a812ef509fcccf107cf38a0faaaff9fa2fec2835
Ian Johnson <ipjohnson123@yahoo.com>
2021-04-09 10:55:16 -0400

    adding test for issue #273

---
+++ tests/Grace.Tests/DependencyInjection/ExtraData/BlackListExtraDataTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grace.DependencyInjection;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.ExtraData
+{
+    public class BlackListExtraDataTests
+    {
+        public class Top
+        {
+            public Top(Sub subInstance)
+            {
+                SubInstance = subInstance;
+            }
+
+            public Sub SubInstance { get;   }
+        }
+
+        public class Sub
+        {
+
+        }
+
+
+        [Fact]
+        public void BlackListModelExtraData()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(block => block.ExcludeTypeFromAutoRegistration(typeof(Sub)));
+
+            var subInstance = new Sub();
+
+            var topInstance = container.Locate<Top>(new {subInstance});
+
+            Assert.Equal(subInstance, topInstance.SubInstance);
+        }
+    }
+}
