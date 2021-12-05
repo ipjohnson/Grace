a013b545a8ac3cb889a6df73a4d000a498e94621
Ian Johnson <ipjohnson123@yahoo.com>
2021-12-05 06:23:27 -0500

    adding keyed import test

---
+++ tests/Grace.Tests/Classes/Simple/KeyedMultipleService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grace.DependencyInjection.Attributes;
+
+namespace Grace.Tests.Classes.Simple
+{
+    public interface IKeyedMultipleService
+    {
+        string SomeMethod();
+    }
+
+
+    [ExportKeyedType(typeof(IKeyedMultipleService), "A")]
+    public class KeyedMultipleServiceA : IKeyedMultipleService
+    {
+        public string SomeMethod()
+        {
+            return "A";
+        }
+    }
+
+    [ExportKeyedType(typeof(IKeyedMultipleService), "B")]
+    public class KeyedMultipleServiceB : IKeyedMultipleService
+    {
+        public string SomeMethod()
+        {
+            return "B";
+        }
+    }
+
+    [Export]
+    public class ImportKeyedMultiple
+    {
+        [Import(Key = "A")]
+        public IKeyedMultipleService ServiceA { get; set; }
+
+        [Import(Key = "B")]
+        public IKeyedMultipleService ServiceB { get; set; }
+    }
+}
---
+++ tests/Grace.Tests/DependencyInjection/AttributeTests/KeyedAttributeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Grace.DependencyInjection;
+using Grace.Tests.Classes.Simple;
+using Xunit;
+
+namespace Grace.Tests.DependencyInjection.AttributeTests
+{
+    public class KeyedAttributeTests
+    {
+        [Fact]
+        public void KeyedAttributeTest()
+        {
+            var container = new DependencyInjectionContainer();
+
+            var assembly = Assembly.GetAssembly(typeof(ImportKeyedMultiple));
+
+            container.Configure(c => c
+                .ExportAssembly(assembly).ExportAttributedTypes().Where(TypesThat.AreInTheSameNamespaceAs(typeof(ImportKeyedMultiple)))
+            );
+
+            var instance = container.Locate<ImportKeyedMultiple>();
+
+            Assert.NotNull(instance);
+            Assert.Equal("A", instance.ServiceA.SomeMethod());
+            Assert.Equal("B", instance.ServiceB.SomeMethod());
+        }
+    }
+}
