7f13aec150011140dab48a9a54081856ae02db3b
jods4 <joel.dumas@elca.ch>
2024-01-03 22:36:16 +0100

    Lifestyles per key for ImportKey.Any

--- tests/Grace.Tests/DependencyInjection/Keyed/AnyKeyTests.cs
+++ tests/Grace.Tests/DependencyInjection/Keyed/AnyKeyTests.cs
@@ -74,8 +74,6 @@ namespace Grace.Tests.DependencyInjection.Keyed
         [Fact]
         public void Singletons_Per_Key()
         {
-            // This test is critical because there is unique handling of the activation delegate in SingeltonPerScopeLifestyle.
-
             var container = new DependencyInjectionContainer();
 
             container.Configure(c =>
@@ -95,6 +93,35 @@ namespace Grace.Tests.DependencyInjection.Keyed
 
             Assert.NotSame(instance1, instance2);
             Assert.Same(instance1, instance3);
+        }
+
+        [Fact]
+        public void SingletonsPerScope_Per_Key()
+        {
+            var container = new DependencyInjectionContainer();
+
+            container.Configure(c =>
+            {
+                c.Export<ImportKeyService>()
+                    .AsKeyed<ImportKeyService>(ImportKey.Any)
+                    .Lifestyle
+                    .SingletonPerScope();
+            });
+
+            var instance1 = container.Locate<ImportKeyService>(withKey: "Key1");
+            Assert.NotNull(instance1);
+            var instance2 = container.Locate<ImportKeyService>(withKey: "Key2");
+            Assert.NotNull(instance2);
+            var instance3 = container.Locate<ImportKeyService>(withKey: "Key1");
+            Assert.NotNull(instance3);
+
+            var scope = container.CreateChildScope();
+            var instance4 = scope.Locate<ImportKeyService>(withKey: "Key1");
+            Assert.NotNull(instance4);
+
+            Assert.NotSame(instance1, instance2);
+            Assert.Same(instance1, instance3);
+            Assert.NotSame(instance1, instance4);
         }
 
         [Fact(Skip = "Locating keyed IEnumerable is not supported yet")]
